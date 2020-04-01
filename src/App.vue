be2d89f8bad82ac26c2b7dcde6c09fc9ff93056c
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-04-01 01:55:39 -0300

    commitando alteraçoes feitas para computed properties

--- src/App.vue
+++ src/App.vue
@@ -130,17 +130,17 @@ export default {
           <div class="card-body">
             <ul class="list-group">
               <li class="list-group-item" v-for="domain in domains" :key="domain.name">
-															<div class="row">
-																<div class="col-md">
-																	{{domain.name}}
-																</div>
-																<div class="col-md text-right">
-																	<a class="btn btn-info" :href="domain.checkout" target="_blank">
-																		<span class="fa fa-shopping-cart"></span>
-																	</a>
-																</div>
-															</div>
-														</li>
+								<div class="row">
+									<div class="col-md">
+										{{domain.name}}
+									</div>
+									<div class="col-md text-right">
+										<a class="btn btn-info" :href="domain.checkout" target="_blank">
+											<span class="fa fa-shopping-cart"></span>
+										</a>
+									</div>
+								</div>
+							</li>
             </ul>
           </div>
         </div>
